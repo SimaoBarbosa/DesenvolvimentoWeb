a56dfbf0db767a2b2ad0a16c10de0147712737a9
SimaoBarbosa <SimaoBarbosa@users.noreply.github.com>
2018-10-17 00:28:11 +0100

    app working

--- TP5/server.js
+++ TP5/server.js
@@ -4,13 +4,13 @@ var fs = require('fs')
 var pug = require('pug')
 
 http.createServer((req,res)=>{
-    var purl = url.parse(req.url)
-    var puburl = /\/pub\//
-    if(purl.pathname == '/'||purl.pathname == '/index'){
+    var ourl = url.parse(req.url)
+    var obraUrl = /\/obra\//
+    if(ourl.pathname == '/'||ourl.pathname == '/index'){
         res.writeHead(200,{'Content-Type': 'text/html'})
-        fs.readFile('./jcrpubs/jcrpubs/index.json','utf-8',(erro,dados) => {
+        fs.readFile('./obras-musicais-json/json/index.json','utf-8',(erro,dados) => {
             if (!erro){
-                res.write(pug.renderFile('index.pug',{ind: JSON.parse(dados)}))
+                res.write(pug.renderFile('index.pug',{obras:JSON.parse(dados)}))
             }
             else
                 res.write('<p><b>Erro: </b> ' + erro + '</p>')
@@ -18,19 +18,19 @@ http.createServer((req,res)=>{
         })
         
     }
-    else if(puburl.test(purl.pathname)){
-        var ficheiro = purl.pathname.split('/')[2]
+    else if(obraUrl.test(ourl.pathname)){
+        var ficheiro = ourl.pathname.split('/')[2]
         res.writeHead(200,{'Content-Type': 'text/html'})
-        fs.readFile('jcrpubs/jcrpubs/'+ficheiro + '.json', (erro,dados) => {
+        fs.readFile('./obras-musicais-json/json/'+ficheiro + '.json' , (erro,dados) => {
             if (!erro)
-                res.write(pug.renderFile('template.pug',{pub:JSON.parse(dados)}))
+                res.write(pug.renderFile('template.pug',{obra:JSON.parse(dados)}))
             else
                 res.write('<p><b>Erro: </b> ' + erro + '</p>')
             res.end()
         })
         
     }
-    else if(purl.pathname == '/w3.css') {
+    else if(ourl.pathname == '/w3.css') {
         res.writeHead(200,{'Content-Type': 'text/css'})
         fs.readFile('estilo/w3.css', (erro,dados) => {
             if (!erro)
@@ -42,10 +42,10 @@ http.createServer((req,res)=>{
     }
     else{
         res.writeHead(200,{'Content-Type': 'text/html'})
-        res.write('<p><b>Erro, pedido desconhecido: </b> ' + purl.pathname + '</p>')
+        res.write('<p><b>Erro, pedido desconhecido: </b> ' + ourl.pathname + '</p>')
         res.end()
     }
 
-}).listen(5000, ()=>{
-    console.log('Servidor á escuta na porta 5000...')
+}).listen(5555, ()=>{
+    console.log('Servidor á escuta na porta 5555...')
 })
